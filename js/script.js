fb5d60ae3dd90d4ed3e5b2be623dc88ba90a075a
Deb <deborah.tsao@gmail.com>
2016-06-29 14:19:59 -0700

    the check button now works as well

--- js/script.js
+++ js/script.js
@@ -13,4 +13,16 @@ $('form').on('submit', function(event){
 	$('ul').on('click', '.x', function(event){event.preventDefault(); $(this).parent().parent().remove()});
 
 
+//duplicate this for the add button v, make the strikeout thicker. and then i think i am done unless
+//i feel like trying to make it draggable...Lol hurr hurr...
+$('ul').on('click', '.cbutton', function(){
+	var clickeditem = $(this).closest('li').text();
+	$('ul').on('click', '.cbutton', function(event){event.preventDefault(); $(this).parent().parent().remove()});
+$('ul').append('<li><div class="checked-item"><img class="x" src="images/x.png"><span>' + clickeditem + '</span><img class="abutton" src="images/add.png"></div></li>');
+
 });
+});
+
+
+//try replaceWith, if not, simplify that .onClick, div background color turns gray, and checkmark turns green, could probably do with a parent thing
+//OMG IT'S WORKING NOW YAY HAHDSFLKSJAFKHSALKFA
