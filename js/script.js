3dd3bf59250ea9eea94d34880b73512e6a2ea7ab
Deb <deborah.tsao@gmail.com>
2016-06-28 23:37:34 -0700

    form input and delete list item works now

--- js/script.js
+++ js/script.js
@@ -5,7 +5,12 @@ $('form').on('submit', function(event){
 	event.preventDefault();
 	var newitem = $('[name="stuff"]').val();
 	console.log(newitem);
+	$('ul').append('<li><div class="list-item"><img class="x" src="images/x.png"><span>' + newitem + '</span><img class="cbutton" src="images/check.png"></div></li>');
+	$('[name="stuff"]').val('');
 });
+
+//delete list items
+	$('ul').on('click', '.x', function(event){event.preventDefault(); $(this).parent().parent().remove()});
 
 
 });
